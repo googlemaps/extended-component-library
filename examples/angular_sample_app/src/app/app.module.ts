3076e82e773f9862e3ade44d1b431a8189f49eaf
Extended Component Library Team <extended-component-library-authors@google.com>
2025-09-02 18:14:21 -0700

    Automated Code Change
    PiperOrigin-RevId: 802349001

--- examples/angular_sample_app/src/app/app.module.ts
+++ examples/angular_sample_app/src/app/app.module.ts
@@ -1,3 +1,5 @@
+// g3-format-prettier
+import {provideZoneChangeDetection} from '@angular/core';
 /**
  * @license
  * Copyright 2023 Google LLC
@@ -9,12 +11,14 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 
+@NgModule({providers: [provideZoneChangeDetection()]})
+export class ZoneChangeDetectionModule {}
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [ZoneChangeDetectionModule, BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule {
-}
+export class AppModule {}
