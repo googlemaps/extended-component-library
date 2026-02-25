bddb510441b4f3794b3eb683bb7834d6d7ee687e
Extended Component Library Team <extended-component-library-authors@google.com>
2026-02-24 17:58:28 -0800

    chore: replace platformBrowserDynamic with platformBrowser
    PiperOrigin-RevId: 874873475

--- examples/angular_sample_app/src/main.ts
+++ examples/angular_sample_app/src/main.ts
@@ -4,11 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {platformBrowser} from '@angular/platform-browser';
 
 import {AppModule} from './app/app.module';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule).catch(err => {
+platformBrowser().bootstrapModule(AppModule).catch(err => {
   console.error(err);
 });
